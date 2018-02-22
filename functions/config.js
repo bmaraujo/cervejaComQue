dd821df808644cfab85571ae7f810ae2808121a5
bruno <bruno.mourao.araujo@gmail.com>
2018-02-22 12:06:50 -0300

    Criado arquivo de configuração

---
+++ functions/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value:      value,
+        enumerable: true
+    });
+};
+
+// suggestions
+define('API_KEY', '');
+define('FIREBASE_DB_USER','');
+define('FIREBASE_DB_PASS','');
